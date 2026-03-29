b5171810e2876e36852f8dbf44cd4ba7c5b6cea3
Sushil Dhakal <sushildhakal1@gmail.com>
2026-03-29 19:12:28 +1100

    change

--- components/scheduling/packages/scheduler/src/Scheduler.tsx
+++ components/scheduling/packages/scheduler/src/Scheduler.tsx
@@ -241,7 +241,7 @@ function SchedulerInner({
           )
         })}
       </div>
-      <div className="flex flex-1 flex-col overflow-hidden">
+      <div className="flex min-h-0 flex-1 flex-col">
         {renderView()}
       </div>
     </div>
